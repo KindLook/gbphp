10ecee37cb8c8eddce9e82e5743dc20012395a0e
provi11 <provi11@yandex.ru>
2020-08-17 16:18:16 +0300

    задание 1 привязал картинки к index, foreach их перебирает и выкладывает

--- lessen4/hw_4/index.php
+++ lessen4/hw_4/index.php
@@ -5,19 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    
 </head>
 <body>
-    <ul class="foto">
-        <li class="list-1"></li>
-        <li class="list-1"></li>
-        <li class="list-1"></li>
-        <li class="list-1"></li>
-        <li class="list-1"></li>
-        <li class="list-1"></li>
-        <li class="list-1"></li>
-    </ul>
-    <?= 
+   
+   <?php  
+   $a = ['<a href="img/champignon.jpg"><img src="img/champignon.jpg" 
+   width="189" alt="#"></a>', '<a href="img/chanterelle.jpg"><img src="img/chanterelle.jpg" 
+   width="189" alt="#"></a>', '<a href="img/Saffron.jpg"><img src="img/Saffron.jpg" 
+   width="189" alt="#"></a>','<a href="img/White.jpg"><img src="img/White.jpg" 
+   width="189" alt="#"></a>',];
+    
+    echo $a[0];
 
-    ?>
+    foreach ($a as $value) {
+        echo $value;
+        
+    };
+
+   ?>
+    
+
 </body>
 </html>
