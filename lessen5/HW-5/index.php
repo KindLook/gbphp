c809a8872f19a8c0de2168603c1dd7d1ccba67a4
provi11 <provi11@yandex.ru>
2020-08-20 13:20:32 +0300

    добавил фото и index.php

---
+++ lessen5/HW-5/index.php
@@ -0,0 +1,48 @@
+<?php
+$link = mysqli_connect('127.0.0.1', 'root', 'root', 'gbphp');   //подключились к БД
+delete($link);
+addData($link);
+
+
+$result = mysqli_query($link, "SELECT * FROM users");   //сделали запрос
+
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo <<<php
+        <h1>Name: {$row['name']}</h1>               
+        <p>id: {$row['id']}</p>                         
+        <a href="?del={$row['id']}">Удалить</a>;    
+        <hr>
+php;
+//заголовок - Имя
+//id
+//создали ссылку на удаление - текст\=id пользователя/при нажатии записываетс яв массив 'del' _GET.
+}
+
+function delete($link){             //функция на удаление пользователя
+    if (empty((int)$_GET['del'])) {  //если массив 'del' в _Get пустой то ничего
+        return;
+    }
+    $id = (int)$_GET['del'];          //иначе в $id = число из _GET
+    $sql = "DELETE FROM `users` WHERE id = {$id}";  //переменная на удаление id
+    mysqli_query($link, $sql);          //отправляем запрос с переменной удаления
+    header('Location: ?');        //оюновляем страницу с нулевым запросом в адресной строке
+    exit;                               //выход из функции
+}
+
+function addData($link)
+{                                        //функция на удаление пользователя
+    if (empty($_GET['addData'])) {  //если массив 'del' в _Get пустой то ничего
+        return;
+    }
+    $login = 'userAdd';
+    $password = '123userAdd';
+    $name = '123userAdd';
+
+    $sql = "INSERT INTO `users` (`login`, `password`, `name`) 
+            VALUES ( '$login', '$password', '$name')";
+
+    mysqli_query($link, $sql);          //отправляем запрос с переменной удаления
+    header('Location: ?');        //оюновляем страницу с нулевым запросом в адресной строке
+    exit;                               //выход из функции
+}
