199e0e01557b3e480527d4141ce51333ccafca12
provi11 <provi11@yandex.ru>
2020-08-20 15:47:50 +0300

    На главной странице все картинки и название

--- lessen5/HW-5/index.php
+++ lessen5/HW-5/index.php
@@ -1,47 +1,25 @@
 <?php
 
 require_once 'parts/header.php';
+
+$products = mysqli_query($link, "SELECT * FROM products");
+$products = mysqli_fetch_all($products, MYSQLI_ASSOC);
+
+
+
 ?>
 
     <div class="main">
+        <? foreach ($products as $product) { ?>
         <div class="card">
             <a href="product.php">
-                <img src="img/amanita.jpg" alt="Фото">
+                <img src="img/<?=$product['img']?>" alt="<?=$product['rus_name']?>">
             </a>
+            <div class="label"><?=$product['rus_name']?></div>
         </div>
-
-        <div class="card">
-            <a href="product.php">
-                <img src="img/amanita.jpg" alt="Фото">
-            </a>
-        </div>
-
-        <div class="card">
-            <a href="product.php">
-                <img src="img/amanita.jpg" alt="Фото">
-            </a>
-        </div>
-
-        <div class="card">
-            <a href="product.php">
-                <img src="img/amanita.jpg" alt="Фото">
-            </a>
-        </div>
-
-
-        <div class="card">
-            <a href="product.php">
-                <img src="img/amanita.jpg" alt="Фото">
-            </a>
-        </div>
-
-
-        <div class="card">
-            <a href="product.php">
-                <img src="img/amanita.jpg" alt="Фото">
-            </a>
-        </div>
+        <? } ?>
     </div>
+
 
 </body>
 </html>
