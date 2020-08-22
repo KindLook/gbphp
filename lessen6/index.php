da59d7706713c15123e36ece690f1db7c33e0b40
provi11 <provi11@yandex.ru>
2020-08-22 15:51:06 +0300

    post отправка

--- lessen6/index.php
+++ lessen6/index.php
@@ -1,20 +1,22 @@
 <?php
-var_dump($_GET);
+//var_dump($_GET);
+var_dump($_POST);
+
 ?>
 
-<h1>Логин: <?= $_GET['user']['login']?></h1>
+<h1>Логин: <?= $_POST['user']['login']?></h1>
 <a href="?user[name]=Саша&user[login]=sasha">Саша</a>   
 <a href="?user[name]=Юля&user[login]=julia">Юля</a>
 <a href="?user[name]=Миша&user[login]=misha">Миша</a>
 <a href="?">Очистить</a>                                
 <br><br>
 
-<form >
+<form method="post" >
     <label>
-        <input name="user[name]" placeholder="name" value="<?= $_GET['user']['login']?>">
+        <input name="user[name]" placeholder="name" value="<?= $_POST['user']['login']?>">
     </label>
     <label>
-        <input name="user[login]" placeholder="login" value="<?= $_GET['user']['login']?>">
+        <input name="user[login]" placeholder="login" value="<?= $_POST['user']['login']?>">
     </label>
     <label>
         <input type="checkbox" name="test[]" value="1" <?= hasChecked(1)?>>
@@ -31,11 +33,11 @@ var_dump($_GET);
 <?
 function hasChecked($id)
 {
-    if(empty($_GET['test'])){
+    if(empty($_POST['test'])){
         return '';
     }
 
-    if(in_array($id, $_GET['test'])){
+    if(in_array($id, $_POST['test'])){
         return 'checked';
     }
 
